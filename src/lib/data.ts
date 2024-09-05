29f8805aedc73777abcb5bfe841883a89f19b91b
ZUBOV ILLIA <zubovillia@gmail.com>
2024-09-05 13:46:08 +0300

    added parents, students

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,7 @@
 // TEMPORARY DATA
 
-export let role = "admin";
-// export let role = "student";
+// export let role = "admin";
+export let role = "student";
 // export let role = "parent";
 // export let role = "teacher ";
 
